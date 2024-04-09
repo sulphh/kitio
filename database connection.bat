5057ca67cf02fbefc5727302aa399958534de1a7
Callum <callumjbuckley@gmail.com>
2024-04-09 19:47:54 +0100

    Remove database connection batch file

--- database connection.bat
+++
@@ -1,3 +0,0 @@
-mongosh "mongodb://localhost/kitio" --username sulphh --password password1234
-mongo atlas user: sulphh pw: mZFhrBv2sCfxcrop
-API key: 65csb5KL4zRao1hEhYBuHnoensn1bfhJOYpc4omM68uUiXyqV9dTg5KaJw02cFxc
